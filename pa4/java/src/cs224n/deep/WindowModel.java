2af5a11501b545aae49406b026cbd93e82dd6fec
Cobra Kao <ru6xul6@gmail.com>
2014-11-29 16:44:33 -0800

    Fix some formatting

--- pa4/java/src/cs224n/deep/WindowModel.java
+++ pa4/java/src/cs224n/deep/WindowModel.java
@@ -10,36 +10,36 @@ import java.text.*;
 
 public class WindowModel {
 
-	protected SimpleMatrix L, W, Wout;
-	//
-	public int windowSize,wordSize, hiddenSize;
+    protected SimpleMatrix L, W, Wout;
+    //
+    public int windowSize,wordSize, hiddenSize;
 
-	public WindowModel(int _windowSize, int _hiddenSize, double _lr){
-		//TODO
-	}
+    public WindowModel(int _windowSize, int _hiddenSize, double _lr){
+        //TODO
+    }
 
-	/**
-	 * Initializes the weights randomly. 
-	 */
-	public void initWeights(){
-		//TODO
-		// initialize with bias inside as the last column
-		// W = SimpleMatrix...
-		// U for the score
-		// U = SimpleMatrix...
-	}
+    /**
+     * Initializes the weights randomly.
+     */
+    public void initWeights(){
+        //TODO
+        // initialize with bias inside as the last column
+        // W = SimpleMatrix...
+        // U for the score
+        // U = SimpleMatrix...
+    }
 
 
-	/**
-	 * Simplest SGD training 
-	 */
-	public void train(List<Datum> _trainData ){
-		//	TODO
-	}
+    /**
+     * Simplest SGD training
+     */
+    public void train(List<Datum> _trainData ){
+        // TODO
+    }
 
-	
-	public void test(List<Datum> testData){
-		// TODO
-		}
-	
+
+    public void test(List<Datum> testData){
+        // TODO
+    }
+
 }
